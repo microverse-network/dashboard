4c1375ffbf075d6caf28aaf1ce1f8477e0ec96a2
Hakan Karadis <hakan@koding.com>
2018-01-02 12:30:07 -0800

    remove unused variables

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react'
 
-import CenteredContainer from 'components/CenteredContainer'
-
 export default class Home extends Component {
   render() {
     return <div>HOME</div>
