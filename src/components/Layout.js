cfe448c71601c9bf8ee9b15eef869d0f859a6dea
Umut Sirin <umutsirin1@gmail.com>
2018-01-04 11:44:39 -0800

    don't render Navbar
    Signed-off-by: Umut Sirin <umutsirin1@gmail.com>

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react'
 
-import Navbar from 'components/Navbar'
 import Sidebar from 'components/Sidebar'
 
 export default class Layout extends Component {
   render() {
     return (
       <div>
-        <Navbar />
         <Sidebar />
         {this.props.children}
       </div>
--- src/pages/SingleApp.js
+++ src/pages/SingleApp.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+
+import { Link } from 'react-router-dom'
+import { Icon, Tab, Container, Menu } from 'semantic-ui-react'
+
 import AppDetails from 'pages/AppDetails'
 import AppLogs from 'pages/AppLogs'
 import AppVersionHistory from 'pages/AppVersionHistory'
-import { Icon, Button, Header, Tab, Container } from 'semantic-ui-react'
 
 const panes = [
   {
@@ -21,6 +24,46 @@ const panes = [
 ]
 
 class SingleApp extends React.Component {
+  static propTypes = {
+    app: PropTypes.shape({
+      _id: PropTypes.string,
+      hash: PropTypes.string,
+      name: PropTypes.string,
+      limit: PropTypes.string,
+      version: PropTypes.string,
+      meta: PropTypes.shape({
+        created: PropTypes.instanceOf(Date),
+      }),
+      labels: PropTypes.array,
+      healthcheck: PropTypes.string,
+      environment: PropTypes.string,
+      region: PropTypes.string,
+      methods: PropTypes.array,
+    }),
+  }
+
+  // TODO: remove
+  static defaultProps = {
+    app: {
+      _id: 'BVqz9Vyv51amjVEzFwXecRa8k023',
+      hash: '0x543AB43A345BCD4D',
+      name: 'Main app',
+      limit: '300 calls/min',
+      version: '2.5',
+      meta: {
+        created: new Date(),
+      },
+      labels: [],
+      healthcheck: 'Running',
+      environment: 'Chrome 63.0',
+      region: 'Node',
+      methods: [
+        { name: 'Square', signature: 'Number, Function' },
+        { name: 'Lookup', signature: 'String | Object, Function' },
+      ],
+    },
+  }
+
   state = {
     activeIndex: 0,
   }
@@ -56,28 +99,21 @@ class SingleApp extends React.Component {
     }
   }
 
-  onBackButtonClick = () => {
-    this.props.history.push('/apps')
-  }
-
   render() {
     const { app } = this.props
     const { activeIndex } = this.state
 
     return (
       <Container fluid>
-        <Button animated onClick={this.onBackButtonClick}>
-          <Button.Content visible>Apps</Button.Content>
-          <Button.Content hidden>
+        <Menu borderless>
+          <Menu.Item inverted as={Link} link to="/apps">
             <Icon name="left arrow" />
-          </Button.Content>
-        </Button>
-        <Header as="h2">
-          {app.name}
-          <Header.Subheader>
-            v{app.version} - {app.limit}
-          </Header.Subheader>
-        </Header>
+            Apps
+          </Menu.Item>
+          <Menu.Item>
+            <Menu.Header as="h2">{app.name}</Menu.Header>
+          </Menu.Item>
+        </Menu>
         <Tab
           activeIndex={activeIndex}
           menu={{ secondary: true, pointing: true }}
@@ -87,45 +123,6 @@ class SingleApp extends React.Component {
       </Container>
     )
   }
-}
-
-SingleApp.propTypes = {
-  app: PropTypes.shape({
-    _id: PropTypes.string,
-    hash: PropTypes.string,
-    name: PropTypes.string,
-    limit: PropTypes.string,
-    version: PropTypes.string,
-    meta: PropTypes.shape({
-      created: PropTypes.instanceOf(Date),
-    }),
-    labels: PropTypes.array,
-    healthcheck: PropTypes.string,
-    environment: PropTypes.string,
-    region: PropTypes.string,
-    methods: PropTypes.array,
-  }),
-}
-
-SingleApp.defaultProps = {
-  app: {
-    _id: 'BVqz9Vyv51amjVEzFwXecRa8k023',
-    hash: '0x543AB43A345BCD4D',
-    name: 'Main app',
-    limit: '300 calls/min',
-    version: '2.5',
-    meta: {
-      created: new Date(),
-    },
-    labels: [],
-    healthcheck: 'Running',
-    environment: 'Chrome 63.0',
-    region: 'Node',
-    methods: [
-      { name: 'Square', signature: 'Number, Function' },
-      { name: 'Lookup', signature: 'String | Object, Function' },
-    ],
-  },
 }
 
 export default SingleApp
