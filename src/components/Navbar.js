184631d10f92e7538d8dfc59191a04c269b40a31
Umut Sirin <umutsirin1@gmail.com>
2018-01-03 20:54:50 -0800

    tweak Sidebar & Navbar
    Signed-off-by: Umut Sirin <umutsirin1@gmail.com>

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,18 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import { Menu, Icon } from 'semantic-ui-react'
-import Logo from 'components/Logo'
 
-export default function Navbar() {
-  return (
-    <Menu fluid fixed="top" style={{ height: 60 }}>
-      <Menu.Item style={{ width: 150 }} as={Link} link to="/">
-        <Logo />
-      </Menu.Item>
-      <Menu.Menu
-        style={{ borderRight: '1px solid rgba(34, 36, 38, 0.1)' }}
-        position="right">
-        <Menu.Item content="Profile" as={Link} link to="/#" position="right" />
-        <Menu.Item as={Link} link to="/#">
-          <Icon name="sign out" />
-        </Menu.Item>
-      </Menu.Menu>
-    </Menu>
-  )
+export default class Navbar extends Component {
+  render() {
+    return (
+      <Menu fixed="top" style={{ height: 60 }}>
+        <Menu.Menu position="right">
+          <Menu.Item content="Profile" as={Link} link to="/" />
+          <Menu.Item as={Link} link to="/#">
+            <Icon name="sign out" />
+          </Menu.Item>
+        </Menu.Menu>
+      </Menu>
+    )
+  }
 }
-
-Navbar.propTypes = {}
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,38 +1,52 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-import { Sidebar, Menu, Icon } from 'semantic-ui-react'
+import { Sidebar as SemanticSidebar, Menu, Icon } from 'semantic-ui-react'
 
-export default function Menubar() {
-  return (
-    <Sidebar
-      as={Menu}
-      visible={true}
-      style={{ top: 60 }}
-      width="thin"
-      icon="labeled"
-      vertical>
-      <Menu.Item name="dashboard" as={Link} link to="/dashboard">
-        <Icon name="dashcube" />
-        Dashboard
-      </Menu.Item>
-      <Menu.Item name="browser" as={Link} link to="/apps">
-        <Icon name="browser" />
-        Your Apps
-      </Menu.Item>
-      <Menu.Item name="key" as={Link} link to="/api-key">
-        <Icon name="key" />
-        Api Key Management
-      </Menu.Item>
-      <Menu.Item name="setting" as={Link} link to="/account-settings">
-        <Icon name="setting" />
-        Account Settings
-      </Menu.Item>
-      <Menu.Item name="payment" as={Link} link to="/payment">
-        <Icon name="payment" />
-        Payment
-      </Menu.Item>
-    </Sidebar>
-  )
+import Logo from 'components/Logo'
+
+export default class Sidebar extends Component {
+  render() {
+    return (
+      <SemanticSidebar
+        as={Menu}
+        vertical
+        inverted
+        visible={true}
+        width="thin"
+        icon="labeled">
+        <Menu.Item
+          as={Link}
+          link
+          to="/"
+          style={{
+            height: 60,
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+          <Logo inverted />
+        </Menu.Item>
+        <Menu.Item as={Link} link to="/dashboard">
+          <Icon name="dashcube" />
+          Dashboard
+        </Menu.Item>
+        <Menu.Item as={Link} link to="/apps">
+          <Icon name="browser" />
+          Your Apps
+        </Menu.Item>
+        <Menu.Item as={Link} link to="/api-key">
+          <Icon name="key" />
+          Api Key Management
+        </Menu.Item>
+        <Menu.Item as={Link} link to="/account-settings">
+          <Icon name="setting" />
+          Account Settings
+        </Menu.Item>
+        <Menu.Item as={Link} link to="/payment">
+          <Icon name="payment" />
+          Payment
+        </Menu.Item>
+      </SemanticSidebar>
+    )
+  }
 }
-
-Menubar.propTypes = {}
